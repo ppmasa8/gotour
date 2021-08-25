d868b1e47a8e3ceb66036c2c029f464d3ea7ab8a
ppmasa8 <kiyokiyopip56@gmail.com>
2021-08-25 17:52:21 +0900

    add

--- packages.go
+++ packages.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
+	//fmt.Println(math.pi)だとエクスポートされていないため、エラーになる
+	fmt.Println(math.Pi)
 }
 
