84420e7f23daf1a95bbf820c8a439a564af9166b
ppmasa8 <kiyokiyopip56@gmail.com>
2021-09-06 21:52:27 +0900

    easy calc

--- calc.go
+++ calc.go
@@ -1,11 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+func calc(arg1 int, arg2 string, arg3 int) string {
+	switch arg2 {
+	case "+":
+		return strconv.Itoa(arg1 + arg3)
+	case "-":
+		return strconv.Itoa(arg1 - arg3)
+	case "*":
+		return strconv.Itoa(arg1 * arg3)
+	case "/":
+		return strconv.Itoa(arg1 / arg3)
+	default:
+		return "Please type + - * / ."
+	}
+}
 
 func main() {
 	var arg1 int
 	var arg2 string
 	var arg3 int
+	fmt.Println("Enter a formula　ex(29 * 9")
 	fmt.Scan(&arg1, &arg2, &arg3)
-	fmt.Println(arg1, arg2, arg3)
+	fmt.Println(calc(arg1, arg2, arg3))
 }
