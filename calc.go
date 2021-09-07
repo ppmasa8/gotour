e6e771c4bc26bf4999de05f546443e46e5936b92
ppmasa8 <kiyokiyopip56@gmail.com>
2021-09-07 15:41:05 +0900

    add

--- calc.go
+++ calc.go
@@ -15,24 +15,35 @@ func valid_int(arg1 int, arg3 int) string {
 		return err
 }
 
-func calc(arg1 int, arg2 string, arg3 int) string {
+func valid_iterator(arg2 string) string {
+	var err = "Enter an +,-,*,/ in the second fields."
+	iterator := [4]string{"+", "-", "*", "/"}
+	if !arrayContains(iterator, arg2) {
+		goto Warning
+	}
+	Warning:
+		return err
+}
+
+func calc(arg1 int, arg2 string, arg3 int) (string, string) {
 	if valid_int(arg1, arg3) != "" {
+		goto Error
+	}
+	if valid_iterator(arg2) != "" {
 		goto Error
 	}
 	switch arg2 {
 	case "+":
-		return strconv.Itoa(arg1 + arg3)
+		return strconv.Itoa(arg1 + arg3), ""
 	case "-":
-		return strconv.Itoa(arg1 - arg3)
+		return strconv.Itoa(arg1 - arg3), ""
 	case "*":
-		return strconv.Itoa(arg1 * arg3)
+		return strconv.Itoa(arg1 * arg3), ""
 	case "/":
-		return strconv.Itoa(arg1 / arg3)
-	default:
-		return "Please type + - * / ."
+		return strconv.Itoa(arg1 / arg3), ""
 	}
 	Error:
-		return valid_int(arg1, arg3)
+		return valid_int(arg1, arg3), valid_iterator(arg2)
 }
 
 func main() {
@@ -41,6 +52,20 @@ func main() {
 	var arg3 int
 	fmt.Println("Enter a formula　ex(29 * 9")
 	fmt.Scan(&arg1, &arg2, &arg3)
+
+	// For Debug
 	fmt.Println(arg1, arg2, arg3)
+	//
+
 	fmt.Println(calc(arg1, arg2, arg3))
+}
+
+// Tools
+func arrayContains(arr [4]string, str string) bool {
+	for _, v := range arr{
+		if v == str{
+			return true
+		}
+	}
+	return false
 }
